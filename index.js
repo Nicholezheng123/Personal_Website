facc0ad35611421e481ebe97ffd5c8ad8f40c748
nz268 <nz268@cornell.edu>
2025-10-18 16:25:44 -0400

    edits to make background appear

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var granimInstance = new Granim({
     direction: 'top-bottom',
     isPausedWhenNotInView: true,
     image : {
-        source: 'assets/cornell.jpg', // intro section background image
+        source: 'assests/cornell.jpg', // intro section background image
         blendingMode: 'multiply',
     },
     states : {
